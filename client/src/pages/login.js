64fec2d72f76a30f8bbe52e507c7674d1f0818a0
tsimpson <travisrsimpson92@gmail.com>
2021-03-16 20:06:51 -0500

    setup redux, work on login

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { login } from '../redux/actions/authAction'
+import { useDispatch} from 'react-redux'
 
 const Login = () => {
+  const initialState = { email: '', password: ''}
+  const [userData, setUserData] = useState(initialState)
+  const { email, password } = userData
+
+  const [typePass, setTypePass] = useState(false)
+
+  const dispatch = useDispatch()
+
+  const handleChangeInput = e => {
+    const {name, value} = e.target
+    setUserData({...userData, [name]:value})
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    dispatch(login(userData))
+  }
+
   return (
-    <div>
-      Login
+    <div className="auth_page">
+      <form onSubmit={handleSubmit}>
+        <h3 className="text-uppercase text-center mb-4">Eternal Friends</h3>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" 
+          aria-describedby="emailHelp" onChange={handleChangeInput} value={email} name="email"/>
+          <small className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <div className="pass">
+            <input type={ typePass? "text" : "password" } className="form-control" id="exampleInputPassword1"
+            onChange={handleChangeInput} value={password} name="password"/>
+            <small onClick={() => setTypePass(!typePass)}>
+              {typePass ? 'Hide' : 'Show'}
+            </small>
+          </div>
+
+        </div>
+        
+        <button type="submit" className="btn btn-dark w-100"
+        disabled={email && password ? false : true}>
+          Login
+        </button>
+
+        <p className="my-2">
+          Don't have an account yet? <Link to="/register" style={{color: "crimson"}}>Sign up here!</Link>
+        </p>
+
+      </form>
     </div>
   )
 }
