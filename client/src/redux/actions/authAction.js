0b23c4a8248b0bcbc1169d8d37f5a92c46904f18
tsimpson <travisrsimpson92@gmail.com>
2021-03-18 17:44:18 -0500

    add loading animation, alert, toast, refresh token storage

--- client/src/redux/actions/authAction.js
+++ client/src/redux/actions/authAction.js
@@ -1,15 +1,14 @@
+import { GLOBALTYPES } from './globalTypes'
 import { postDataAPI } from '../../utils/fetchData'
 
-export const TYPES = {
-  AUTH: 'AUTH'
-}
+
 
 export const login = (data) => async (dispatch) => {
   try {
-    dispatch({ type: 'NOTIFY', payload: {loading: true} })
+    dispatch({ type: GLOBALTYPES.ALERT, payload: {loading: true} })
     const res = await postDataAPI('login', data)
     dispatch({
-      type: 'AUTH', 
+      type: GLOBALTYPES.AUTH, 
       payload: {
         token: res.data.access_token,
         user: res.data.user
@@ -18,7 +17,7 @@ export const login = (data) => async (dispatch) => {
 
     localStorage.setItem("firstLogin", true)
     dispatch({
-      type: 'NOTIFY', 
+      type: GLOBALTYPES.ALERT, 
       payload: {
         success: res.data.msg
        } 
@@ -26,10 +25,35 @@ export const login = (data) => async (dispatch) => {
 
   } catch (err) {
     dispatch({
-       type: 'NOTIFY', 
+       type: GLOBALTYPES.ALERT, 
        payload: {
          error: err.response.data.msg
         } 
       })
+  }
+}
+
+export const refreshToken = () => async (dispatch) => {
+  const firstLogin = localStorage.getItem("firstLogin")
+  if(firstLogin){
+    dispatch({ type: GLOBALTYPES.ALERT, payload: {loading: true } })
+    try {
+      const res = await postDataAPI('refresh_token')
+      dispatch({
+        type: GLOBALTYPES.AUTH, 
+        payload: {
+          token: res.data.access_token,
+          user: res.data.user
+         } 
+       })
+       dispatch({ type: GLOBALTYPES.ALERT, payload: {} })
+    } catch (err) {
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: {
+          error: err.response.data.msg
+        }
+      })
+    }
   }
 }
